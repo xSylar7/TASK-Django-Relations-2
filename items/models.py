3540044b1086240dd29998ac3e7213886989b498
Qassem Alsaffar <qas.alsaffar@gmail.com>
2022-10-20 09:08:53 +0300

    created relations on Items, fixed the item view and html and form pages

--- items/models.py
+++ items/models.py
@@ -1,23 +1,27 @@
 from email.policy import default
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
     class Meta:
         verbose_name_plural = "Categories"
-    
+
     name = models.CharField(max_length=150)
     image = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"Category: {self.name}"
 
+
 class Item(models.Model):
     name = models.CharField(max_length=150)
     image = models.TextField()
     price = models.FloatField()
-  
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="items")
 
     def __str__(self):
         return self.name
--- items/views.py
+++ items/views.py
@@ -1,9 +1,13 @@
+from email.mime import image
+from unicodedata import category
 from django.shortcuts import redirect, render
 
 from items.forms import ItemForm
 from .models import Item
 
 # Create your views here.
+
+
 def get_items(req):
     items = Item.objects.all()
     _items = []
@@ -14,21 +18,26 @@ def get_items(req):
                 "name": item.name,
                 "price": item.price,
                 "image": item.image,
+                "category": {
+                    "name": item.category.name,
+                    "image": item.category.image
+                }
             }
         )
     context = {"items": _items}
     return render(req, "item_list.html", context)
 
+
 def get_item(req, item_id):
     item = Item.objects.get(id=item_id)
     context = {
-               "item": { 
-                    "id": item.id,
-                    "name": item.name,
-                    "price": item.price,
-                    "image": item.image
-                }
-            }
+        "item": {
+            "id": item.id,
+            "name": item.name,
+            "price": item.price,
+            "image": item.image
+        }
+    }
     return render(req, "item_detail.html", context)
 
 
